c73d996c19ba65fc09867bcb7722e1208d3f97c8
Shin Thant <shinthant10684@gmail.com>
2023-05-23 16:05:30 +0630

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3500;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "welome!" });
